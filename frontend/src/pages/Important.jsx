d8aefb5e067194e6fc0a6535c38184d0a292b77c
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-11-22 00:50:31 +0100

    first commit

---
+++ frontend/src/pages/Important.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useState } from 'react'
+import { BrowserRouter as Router,  Link } from "react-router-dom"
+import '../App.css'
+
+function Important() {
+  const [notes, setNotes] = useState([
+    {
+      "Title": "My first job",
+      "Content": "lorem lorem lorem lorem lorem",
+      "Category": "Work",
+      "Date": "01.01.2022"
+    },
+    {
+      "Title": "My first job",
+      "Content": "lorem lorem lorem lorem lorem",
+      "Category": "Work",
+      "Date": "01.01.2022"
+    },
+  ])
+
+  return (
+    <div className="App">
+      <header>
+        <h2>My Notes App</h2>
+        <div className='search-bar'>
+        <input placeholder='Search your notes'></input>
+        <button>Search</button>
+        </div>
+      </header>
+      <div className='note-content'>
+      <div className='sidebar'>
+        <h3>All Notes</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+      </div>
+      <main>
+        <div >
+        <ul className='category-navbar'>
+            <Link to={'/work'}>Work</Link>
+            <Link to={'/personal'}>Personal</Link>
+            <Link to={'/fun'}>Fun</Link>
+            <Link to={'/important'}>Important</Link>
+          </ul>
+        </div>
+        <div className='notes'>
+          {notes.map (note => (
+            <>
+              <h3>{note.Title}</h3>
+              <p>{note.Content}</p>
+              <p>{note.Date}</p>
+            </>
+          ) )}
+        </div>
+      </main>
+      </div>
+    </div>
+  )
+}
+
+export default Important
