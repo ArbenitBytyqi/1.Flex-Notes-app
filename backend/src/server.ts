52061c08539e73dee85a38643c2c13e510f17a66
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-11-22 02:01:30 +0100

    server done

---
+++ backend/src/server.ts
@@ -0,0 +1,88 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import cors from "cors";
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
+
+app.get("/notes", async (req, res) => {
+    try {
+      const notes = await prisma.note.findMany({
+      });
+      res.send(notes);
+    } catch (error) {
+      // @ts-ignore
+      res.status(500).send({ error: error.message });
+    }
+  });
+
+  app.get("/notes/:category", async (req, res) => {
+    try {
+      const { category } = req.params;
+  
+      const notes = await prisma.note.findMany({
+        where: { category: category },
+      });
+  
+      res.send(notes);
+    } catch (error) {
+      // @ts-ignore
+      res.status(500).send({ error: error.message });
+    }
+  });
+
+  app.delete("/notes/:id", async (req, res) => {
+    try {
+      const note = await prisma.note.delete({
+        where: { id: Number(req.params.id) },
+      });
+      res.send(note);
+    } catch (error) {
+      res.status(400).send({ error: error });
+    }
+  });
+
+  app.post("/notes", async (req, res) => {
+    const notes = {
+      title: req.body.title,
+      content: req.body.content,
+      category: req.body.category,
+    };
+    let errors: string[] = [];
+  
+    if (typeof req.body.title !== "string") {
+      errors.push("Add a proper title!");
+    }
+    if (typeof req.body.content !== "string") {
+      errors.push("Add a proper note description!");
+    }
+    if (typeof req.body.category !== "string") {
+      errors.push("Select a proper category");
+    }
+    if (errors.length === 0) {
+      try {
+        const newNote = await prisma.note.create({
+          data: {
+            title: notes.title,
+            content: notes.content,
+            category: notes.category,
+          },
+        });
+        res.send(newNote);
+      } catch (err) {
+        // @ts-ignore
+        res.status(400).send(err.message);
+      }
+    } else {
+      res.status(400).send({ errors: errors });
+    }
+  });
+  
+
+app.listen(port, () => {
+    console.log(`Listening to http://localhost:${port}`);
+  });
